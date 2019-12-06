0788b291200ecbf0e617936adfcb770e7f87e4a6
saruit0105 <saruits2@gmail.com>
2019-12-05 23:03:06 -0500

    done

--- starter-code/routes/UserRoutes.js
+++ starter-code/routes/UserRoutes.js
@@ -3,50 +3,45 @@ const router = express.Router();
 const User = require("../models/Users");
 const bcrypt = require("bcryptjs");
 
-router.get("/users/signup", async (_, res, next) => {
-  await res.render("user-views/signup");
-});
+router.get("/users/signup", async (_, res) => res.render("user-views/signup"));
 
 router.post("/signup", async (req, res, next) => {
   try {
+    const { body } = req;
     const salt = bcrypt.genSaltSync(10);
-    const hash = bcrypt.hashSync(req.body.password, salt);
-    const username = req.body.username;
-    User.create({ username: username, password: hash });
+    const password = bcrypt.hashSync(body.password, salt);
+    const username = body.username;
+    User.create({ username, password });
     await res.redirect("/users/login");
   } catch (err) {
     next(err);
   }
 });
 
-router.get("/users/login", async (req, res, next) => {
-  await res.render("user-views/login");
+router.get("/users/login", async (req, res) => {
+  const { error } = req.query;
+  await res.render("user-views/login", {
+    errorMessage: error
+  });
 });
 
 router.post("/users/login", async (req, res, next) => {
   try {
-    const theUsername = req.body.username;
-    const thePassword = req.body.password;
-    const user = await User.findOne({ username: theUsername });
-    if (!user) {
-      alert("USER DOES NOT EXIST!");
-      res.redirect("/users/login");
-      return;
-    }
-    if (bcrypt.compareSync(thePassword, user.password)) {
-      req.session.currentUser = user;
-      res.redirect("/");
-    } else {
-      res.render("users-views/login", {
-        errorMessage: "Incorrect password"
-      });
-    }
+    const { username, password } = req.body;
+    const redirectURL = "/users/login";
+    const user = await User.findOne({ username });
+    if (!user) return res.redirect(redirectURL + "?error=Invalid user");
+    const isAuthenticated = bcrypt.compareSync(password, user.password || "");
+    if (!isAuthenticated)
+      return res.redirect(redirectURL + "?error=Incorrect password");
+    req.session.currentUser = user;
+    res.redirect("/");
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/logout", (req, res, next) => {
+router.post("/logout", (req, res) => {
   req.session.destroy();
   res.redirect("/");
 });
